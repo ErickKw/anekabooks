405bdab682b4fa4fb80037dc3e2fd06547a233b5
ErickKw <erickkusumaw@gmail.com>
2023-03-17 20:04:39 +0700

    first Commit

---
+++ src/components/Header.vue
@@ -0,0 +1,9 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+   <nav class="navbar bg-body-tertiary">
+    <div class="container-fluid">
+      <span class="navbar-brand mb-0 h1">AnekaBooks</span>
+      <span class="navbar-brand mb-0 h1">Cart (0)</span>
+    </div>
+  </nav>
+</template>
